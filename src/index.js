84abfe18a8d3f84d72e22c2caa3070e7d13f9ba6
John Veldboom <john_veldboom@discovery.com>
2023-02-06 20:42:14 -0500

    fix output variable

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const run = async () => {
   // return a default version if no previous github tags
   if (!latestTag) {
     const incrementedVersion = semver.inc('0.0.0', core.getInput('default-bump'))
-    return utils.setVersionOutputs(incrementedVersion, core.getInput('version-prefix'))
+    return utils.setVersionOutputs(incrementedVersion, core.getInput('prefix'))
   }
 
   if (!semver.valid(latestTag.name)) {
@@ -31,7 +31,7 @@ const run = async () => {
   const bump = await utils.getVersionBump(commits, core.getInput('default-bump'))
 
   const incrementedVersion = semver.inc(latestTag.name, bump)
-  utils.setVersionOutputs(incrementedVersion, core.getInput('version-prefix'))
+  utils.setVersionOutputs(incrementedVersion, core.getInput('prefix'))
 }
 
 run()
